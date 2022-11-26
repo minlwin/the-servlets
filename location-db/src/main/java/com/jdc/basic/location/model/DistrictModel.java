4552577f0c04a5fd7e4c4f2f01805c4e906d60b1
Min Lwin <lwin.zawmin@gmail.com>
2022-11-26 10:07:55 +0630

    Update DistrictModel

--- location-db/src/main/java/com/jdc/basic/location/model/DistrictModel.java
+++ location-db/src/main/java/com/jdc/basic/location/model/DistrictModel.java
@@ -2,4 +2,11 @@ package com.jdc.basic.location.model;
 
 public interface DistrictModel {
 
+	// Create Method
+	
+	// Update Method
+	
+	// Find By Id
+	
+	// Search Method - state id, name (like search)
 }
