bfb5494b6fe6dafcee9f5a755a89c9e6cac53701
Min Lwin <lwin.zawmin@gmail.com>
2022-11-26 12:03:50 +0630

    Update SQL

--- location-db/database.sql
+++ location-db/database.sql
@@ -6,6 +6,8 @@ grant all privileges on location_db.* to 'location'@'localhost';
 
 use location_db
 
+drop table if exists state;
+
 create table state(
 	id int primary key auto_increment,
 	name varchar(40) unique not null,
@@ -13,7 +15,20 @@ create table state(
 	capital varchar(20) not null
 );
 
-insert into state(name, region, capital) values ('Ayeyarwady Region', 'Lower', 'Pathein');
-insert into state(name, region, capital) values ('Bago Region', 'Lower', 'Bago');
-insert into state(name, region, capital) values ('Chin State', 'West', 'Hakha');
-insert into state(name, region, capital) values ('Kachin State', 'North', 'Myitkyina');
+drop table if exists district;
+
+create table district(
+	id int primary key auto_increment,
+	name varchar(40) not null,
+	state_id int not null,
+	foreign key(state_id) references state(id)
+);
+
+drop table if exists township;
+
+create table township(
+	id int primary key auto_increment,
+	name varchar(40) not null,
+	district_id int not null,
+	foreign key(district_id) references district(id)
+);
